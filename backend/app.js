2a9b9ebe27d7965cf1341a6bf90423a044b00511
Chris <1988cpaul@gmail.com>
2025-05-25 11:48:24 +0100

    Add app.listen method to backend/app.js Prettier formatting

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,16 @@
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require("sqlite3").verbose();
+const express = require("express");
+const app = express();
+const port = 3000;
 
-const db = new sqlite3.Database('../../db/recipes', (err) => {
-    if (err) {
-        console.error('Could not connect to database', err);
-    }
-    else {
-        console.log('Connected to the database');
-    }
-})
+const db = new sqlite3.Database("../../db/recipes", (err) => {
+  if (err) {
+    console.error("Could not connect to database", err);
+  } else {
+    console.log("Connected to the database");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Recipe app backend listening on port ${port}`);
+});
