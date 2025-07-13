afd7236d90069a859743ab330ddb3f0d92c13801
Chris <1988cpaul@gmail.com>
2025-07-13 22:54:03 +0100

    Add GET route for fetching a recipe by ID

--- backend/src/routes/recipe-routes.js
+++ backend/src/routes/recipe-routes.js
@@ -3,8 +3,9 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const router = express.Router();
 
+const dbPath = path.join(__dirname, '../data/recipes.db');
+
 router.get('/', (req, res) => {
-    const dbPath = path.join(__dirname, '../data/recipes.db');
     
     const db = new sqlite3.Database(dbPath, (err) => {
         if (err) {
@@ -22,6 +23,29 @@ router.get('/', (req, res) => {
             db.close();
         });
     });
-})
+});
+
+router.get('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const db = new sqlite3.Database(dbPath, (err) => {
+        if (err) {
+            console.error("Error connecting to the database", err);
+            res.status(500).send("Internal Server Error");
+            return;
+        }
+        db.get(`SELECT *
+                FROM mock_recipes
+                WHERE id = ${id}`, [], (err, row) => {
+            if (err) {
+                console.error("Error fetching recipes", err);
+                res.status(404).send("Internal Server Error");
+                return;
+            }
+            res.status(200).json(row);
+            db.close();
+        });
+    });
+});
 
 module.exports = router;
