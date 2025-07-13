3a9cfaa85739b2ea5f2fe39c52ec0bb2b47bf01b
Chris <1988cpaul@gmail.com>
2025-07-13 21:45:24 +0100

    Update database connection path and add sqlite3 dependency

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,13 @@
 const sqlite3 = require("sqlite3").verbose();
 const express = require("express");
+
 const app = express();
 const port = 3000;
 
-const db = new sqlite3.Database("../../db/recipes", (err) => {
+const path = require("path");
+const dbPath = path.join(__dirname, "data", "recipes.db");
+
+const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
     console.error("Could not connect to database", err);
   } else {
