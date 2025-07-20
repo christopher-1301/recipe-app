efdfdd9f753caddaddb0e601e42f3dd73133816d
Chris <1988cpaul@gmail.com>
2025-07-20 22:33:26 +0100

    Initialize React app with TypeScript and Vite setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    return (
+        <div>
+            <h1>Recipe App</h1>
+            <p>I am using TypeScript for the first time!</p>
+        </div>
+    )
+}
+export default App
